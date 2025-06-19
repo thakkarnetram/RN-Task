eb44c9e15ef8f4f58919daf3845bc0c31f8b4b2d
thakkarnetram <thakkarnetram10@gmail.com>
2025-06-19 11:26:29 +0530

    bases file

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
